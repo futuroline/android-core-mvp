5710cc5dc5a17842b56c256291b7ab4bc99b670b
Kang <zhoukang@malalaoshi.com>
2016-12-23 18:52:58 +0800

    define AuthInterceptor

--- core/src/main/java/com/comm/android/core/network/NetworkClient.java
+++ core/src/main/java/com/comm/android/core/network/NetworkClient.java
@@ -1,9 +1,17 @@
 package com.comm.android.core.network;
 
+import com.comm.android.core.network.Interceptors.AuthInterceptor;
 import com.comm.android.core.network.api.BaseApi;
 
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Cache;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by kang on 16/12/9.
@@ -71,34 +79,58 @@ Retrofit mRetrofit = new Retrofit.Builder()
 service = mRetrofit.create(YourApi.class);
  */
 
-                   OkHttpClient.Builder builder = new OkHttpClient.Builder();
-                   /**
+            /*       OkHttpClient.Builder builder = new OkHttpClient.Builder();
+                   *//**
                     *设置缓存，代码略
-                    */
+                    *//*
 
-                   /**
+                   *//**
                     *  公共参数，代码略
-                    */
+                    *//*
 
-                   /**
+                   *//**
                     * 设置头，代码略
-                    */
+                    *//*
 
-                   /**
+                   *//**
                     * Log信息拦截器，代码略
-                    */
+                    *//*
 
-                   /**
+                   *//**
                     * 设置cookie，代码略
-                    */
+                    *//*
 
-                   /**
+                   *//**
                     * 设置超时和重连，代码略
-                    */
+                    *//*
                    //以上设置结束，才能build(),不然设置白搭
                    OkHttpClient okHttpClient = builder.build();
                    mRetrofit = new Retrofit.Builder()
                            .baseUrl(BaseApi.getHost())
+                           .client(okHttpClient)
+                           .build();*/
+                   AuthInterceptor authInterceptor = new AuthInterceptor();
+                   OkHttpClient.Builder builder = new OkHttpClient.Builder();
+                   builder.connectTimeout(30, TimeUnit.SECONDS);
+                   //设置拦截器，以用于自定义Cookies的设置
+                   builder.addNetworkInterceptor(new HttpLoggingInterceptor());
+                   //增加认证拦截器
+                   builder.addNetworkInterceptor(authInterceptor);
+                   //设置缓存目录
+                  /* File cacheDirectory = new File(context.getCacheDir()
+                           .getAbsolutePath(), "HttpCache");
+                   Cache cache = new Cache(cacheDirectory, 20 * 1024 * 1024);
+                   builder.cache(cache);*/
+                   OkHttpClient okHttpClient = builder.build();
+                   //构建Retrofit
+                   mRetrofit = new Retrofit.Builder()
+                           //配置服务器路径
+                           .baseUrl(BaseApi.getHost())
+                           //配置转化库，默认是Gson
+                           .addConverterFactory(GsonConverterFactory.create())
+                           //配置回调库，采用RxJava
+                           .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                           //设置OKHttpClient为网络客户端
                            .client(okHttpClient)
                            .build();
                }
